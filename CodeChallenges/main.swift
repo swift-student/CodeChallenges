343a519ce12bd531204d27e008d8e73aaba098af
Shawn Gee <shawngee@gmail.com>
2020-08-04 10:35:56 -0400

    Added project

---
+++ CodeChallenges/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeChallenges
+//
+//  Created by Shawn Gee on 8/4/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
