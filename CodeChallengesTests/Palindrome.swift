f7d1db62efbde718a50c3f7b386661fddafb88e6
Shawn Gee <shawngee@gmail.com>
2020-08-05 23:56:40 -0400

    Added link to Palindrome challenge

--- CodeChallengesTests/Palindrome.swift
+++ CodeChallengesTests/Palindrome.swift
@@ -10,6 +10,8 @@ import XCTest
 
 // MARK: - Challenge
 
+//https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3411/
+
 //Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 //
 //Note: For the purpose of this problem, we define empty string as valid palindrome.
